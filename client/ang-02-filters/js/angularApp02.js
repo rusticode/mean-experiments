e649fcba21855a59f8146d9002175586eda222db
Marek Karwowski <marek@rusticode.com>
2014-06-24 22:12:11 +0100

    custom filter : capitalize added

--- client/ang-02-filters/js/angularApp02.js
+++ client/ang-02-filters/js/angularApp02.js
@@ -1,4 +1,15 @@
+
+
+
 var angularApp02 = angular.module("angularApp02", ['ngResource']);
+angularApp02.filter("capitalize", function() {
+    return function(string) {
+        var regExp = /\b[a-z](?=[a-z]{2})/g;
+        return string.replace(regExp, function(char) {
+            return char.toUpperCase();
+        });
+    }
+})
 
 angularApp02.controller("mainPageController", function($scope, ItemProxy) {
     $scope.title = "Search Engine List";
